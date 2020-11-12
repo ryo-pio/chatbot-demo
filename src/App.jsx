41df3608dddf132c9a0e823357d00f8b70e968c8
ryo <ryosakoi21@icloud.com>
2020-11-13 01:38:31 +0900

    practice#6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css';
-import {AnswersList} from './components';
+import {AnswersList, Chats} from './components/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,6 +12,42 @@ export default class App extends React.Component {
       currentId: "init",
       dataset: defaultDataset,
       open: false
+    }
+    this.selectAnswer = this.selectAnswer.bind(this);
+  }
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
+
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
+    })
+  }
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === "init"):
+        this.displayNextQuestion(nextQuestionId);
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: "answer"
+        })
+
+        this.setState({
+          chats: chats
+        })
+
+        this.displayNextQuestion(nextQuestionId);
+        break;
     }
   }
 
@@ -24,14 +60,16 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.initAnswer()
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId)
   }
 
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          <AnswersList answers={this.state.answers} />
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme: Theme) =>
 const Answer = (props) => {
   // const classes = useStyles();
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
       {props.content}
     </Button>
   )
